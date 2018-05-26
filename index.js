77399dcacbf0ea4fd907c329e01d8f6e2c925271
Simon <simon.siefke@gmail.com>
2018-05-26 14:49:41 +0200

    fix outline showing

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ document.getElementsByTagName('head')[0].appendChild(styleElement)
 const hideOutline = () =>
   (styleElement.innerHTML = ':focus{outline:0}::-moz-focus-inner{border:0;}')
 const showOutline = () => (styleElement.innerHTML = '')
-window.addEventListener('mousedown', showOutline)
-window.addEventListener('keydown', hideOutline)
+window.addEventListener('mousedown', hideOutline)
+window.addEventListener('keydown', showOutline)
